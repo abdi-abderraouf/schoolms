f32a1ab7153db76e13b304abf5c0e29e06eeb507
Hassan El Anabi <helanabi@gmail.com>
2022-10-07 09:28:17 +0100

    highlight current navbar link

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faGraduationCap,
@@ -8,6 +8,9 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
+
+    const path = useLocation().pathname;
+     
     const ulStyle = {
 	listStyleType: "none",
 	display: "flex",
@@ -17,33 +20,35 @@ export default function NavBar() {
 	margin: 0
     };
 
-    const linkStyle = {
+    const linkStyle = to => ({
 	display: "inline-block",
 	fontSize: "1.1rem",
 	padding: ".6em",
 	color: "white",
-    };
-    
+	backgroundColor: path.startsWith(to) ?
+	    "var(--light-trans)" : null
+    });
+
     return (
 	<nav>
 	    <ul style={ ulStyle }>
 		<li>
-		    <Link to="/classes" style={ linkStyle }>
+		    <Link to="/classes" style={ linkStyle("/classes") }>
 			<FontAwesomeIcon icon={ faGraduationCap } /> Classes
 		    </Link>
 		</li>
 		<li>
-		    <Link to="/students" style={ linkStyle }>
+		    <Link to="/students" style={ linkStyle("/students") }>
 			<FontAwesomeIcon icon={ faUsers } /> Students
 		    </Link>
 		</li>
 		<li>
-		    <Link to="/subjects" style={ linkStyle }>
+		    <Link to="/subjects" style={ linkStyle("/subjects") }>
 			<FontAwesomeIcon icon={ faBook } /> Subjects
 		    </Link>
 		</li>
 		<li>
-		    <Link to="/teachers" style={ linkStyle }>
+		    <Link to="/teachers" style={ linkStyle("/teachers") }>
 			<FontAwesomeIcon icon={ faUserTie } /> Teachers
 		    </Link>
 		</li>
