8ea3884e58661d72d55e6f2404a1633940617040
Hassan El Anabi <helanabi@gmail.com>
2022-10-08 15:41:02 +0100

    minor typographic changes to About component

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -13,7 +13,7 @@ export default function About() {
     const colStyle = {
 	padding: "0 2em",
 	width: "45%",
-	textAlign: "center"
+	textAlign: "left"
     };
 	
     return (
@@ -30,22 +30,32 @@ export default function About() {
 	    <div className="flex resp-flex" style={{ justifyContent: "space-between" }}>
 		<div className="card" style={ colStyle }>
 		    <h3>For clients</h3>
-		    <p>
-			It can be customized/extended to meet your specific requirements.<br/>
-			It can be installed locally (no extra charges, e.g. hosting),
-			or deployed online to be accessible from anywhere (charges apply).
-		    </p>
+		    <ul>
+			<li>
+			    Can be customized/extended to meet your specific requirements.
+			</li>
+			<li>
+			    Can be installed locally (no extra charges, e.g. hosting),
+			    or deployed online to be accessible from anywhere (charges apply).
+			</li>
+		    </ul>
 		    <p>
 			
 		    </p>
 		</div>
 		<div className="card" style={ colStyle }>
 		    <h3>For developers</h3>
-		    <p>
-			This is free software (source code).
-			You may request support for customization, deployment, maintenance, etc.<br/>
-			Stack used: <em>ReactJS</em>, <em>ExpressJS</em>, <em>MongoDB</em>.
-		    </p>
+		    <ul>
+			<li>
+			    This is free software (source code).
+			</li>
+			<li>
+			    You may request support for customization, deployment, maintenance, etc.
+			</li>
+			<li>
+			    Stack used: <em>ReactJS</em>, <em>ExpressJS</em>, <em>MongoDB</em>.
+			</li>
+		    </ul>
 		</div>
 	    </div>
 	</div>
